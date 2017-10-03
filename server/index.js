f0d2a62bc9b570d12552ee98b82499ea5f6c5919
Vasiliy Shubko <flit77@gmail.com>
2017-10-03 21:35:27 +0200

    bodyparser added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
 const articlesRoutes = require('./routes/articles.routes');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const PORT = process.env.PORT || 8081;
 
+app.use(bodyParser());
 app.use(articlesRoutes.routes());
 
 const server = app.listen(PORT).on('error', err => {
